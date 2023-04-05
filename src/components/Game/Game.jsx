8310454aad0ad7f6719c7e3db32db94f5c14874b
Руденко  Владислав Сергійович <rvs07@i.ua>
2023-04-05 15:26:57 +0300

    1

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,7 +1,14 @@
 import { questions } from '../../helpers/questions';
 
-export function Game({question, onCliclVariant, step}) {
+export function Game({ question, onCliclVariant, step, backColor }) {
     const percentage = Math.round(step / questions.length * 100);
+   const unCorretcStyle = {
+        backgroundColor: "#ffea66",
+    };
+     const corretcStyle = {
+        backgroundColor: "aqua",
+        transform: "scale(1.07)",
+    };
     return (
         <>
             <div className="progress">
@@ -10,7 +17,7 @@ export function Game({question, onCliclVariant, step}) {
             <h1>{question.title}</h1>
             <ul>
                 {
-                    question.variants.map((item, index) => <li key={index} onClick={()=> {onCliclVariant(index)}}>{item}</li>)
+                    question.variants.map((item, index) => <li style={index === question.correct && backColor ? corretcStyle : unCorretcStyle} key={index} onClick={()=> {onCliclVariant(index)}}>{item}</li>)
                 }
             </ul>
         </>
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -3,21 +3,27 @@ import { useState } from "react";
 import { Result } from "../Result/Result";
 import { Game } from "../Game/Game";
 import { questions } from '../../helpers/questions';
+
+
 export function Quiz() {
     const [step, setStep] = useState(0);
     const [correct, setCorrect] = useState(0);
+    const [backColor, setBackColor] = useState(false);
     const question = questions[step];
+
     const onCliclVariant = (index) => {
-        setStep(step + 1)
+        setTimeout(()=> {setStep(step + 1)}, 500)
         if (index === question.correct) { 
       setCorrect(correct + 1)
-    }
+      setBackColor(true)
+    } 
+    setTimeout(()=> {setBackColor(false)}, 400)
     };
    
     return (
         <div className='cont'>
             <div className="box">
-                {step !== questions.length ? <Game step={step} question={question} onCliclVariant={onCliclVariant} /> : <Result correct={correct} />
+                {step !== questions.length ? <Game backColor={backColor} step={step} question={question} onCliclVariant={onCliclVariant} /> : <Result correct={correct} />
                 }
             </div>
         </div>
