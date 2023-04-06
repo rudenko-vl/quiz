b170db54b1445d2ab3c3179179f588f795f061d6
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2023-04-06 15:24:07 +0300

    clicked style

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,25 +1,30 @@
 import { questions } from '../../helpers/questions';
 
-export function Game({ question, onClickVariant, step, backColor }) {
-    const percentage = Math.round(step / questions.length * 100);
-   const unCorrectStyle = {
-        backgroundColor: "#ffea66",
-    };
-     const correctStyle = {
-        backgroundColor: "aqua",
-        transform: "scale(1.07)",
-    };
-    return (
-        <>
-            <div className="progress">
-                <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
-            </div>
-            <h1>{question.title}</h1>
-            <ul>
-                {
-                    question.variants.map((item, index) => <li style={index === backColor ? correctStyle : unCorrectStyle} key={index} onClick={()=> {onClickVariant(index)}}>{item}</li>)
-                }
-            </ul>
-        </>
-    );
+export function Game({ question, onClickVariant, step, clicked, clickedId }) {
+  const percentage = Math.round((step / questions.length) * 100);
+
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((item, index) => (
+          <li
+            style={index === clickedId ? clicked : null}
+            key={index}
+            onClick={() => {
+              onClickVariant(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -6,27 +6,53 @@ import { questions } from '../../helpers/questions';
 
 
 export function Quiz() {
-    const [step, setStep] = useState(0);
-    const [correct, setCorrect] = useState(0);
-    const [backColor, setBackColor] = useState(null);
-    const question = questions[step];
+  const unCorrectStyle = {
+    backgroundColor: "pink"
+  };
+  const correctStyle = {
+    backgroundColor: "aqua",
+    transform: "scale(1.05)"
+  };
 
-    const onClickVariant = (index) => {
-        setBackColor(index)
-        setTimeout(()=> {setStep(step + 1)}, 500)
-        if (index === question.correct) { 
-      setCorrect(correct + 1)
-    } 
-    setTimeout(()=> {setBackColor(null)}, 400)
-    };
-   
-    return (
-        <div className='cont'>
-            <div className="box">
-                {step !== questions.length ? <Game backColor={backColor} step={step} question={question} onClickVariant={onClickVariant} /> : <Result correct={correct} />
-                }
-            </div>
-        </div>
-    );
-};
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [clicked, setClicked] = useState(null);
+  const [clickedId, setClickedId] = useState(null);
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    setClickedId(index);
+    setTimeout(() => {
+      setStep(step + 1);
+    }, 500);
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+      setClicked(correctStyle);
+    } else {
+      setClicked(unCorrectStyle);
+    }
+    setTimeout(() => {
+      setClicked(null);
+      setClickedId(null);
+    }, 400);
+  };
+
+  return (
+    <div className="cont">
+      <div className="box">
+        {step !== questions.length ? (
+          <Game
+            clicked={clicked}
+            step={step}
+            question={question}
+            onClickVariant={onClickVariant}
+            clickedId={clickedId}
+          />
+        ) : (
+          <Result correct={correct} />
+        )}
+      </div>
+    </div>
+  );
+}
 
